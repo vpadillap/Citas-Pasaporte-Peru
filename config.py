3c3a9509754de18df7d14cc0af196e86e80238e7
vpadillap <158070557+vpadillap@users.noreply.github.com>
2024-11-12 10:52:56 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TwoCaptchaAPI = "123456789"
+dni = "987654321" #el sistema de consulta de citas exige ingresar un dni asi sea solo para hacer la consulta
+
+sedes = ["BLV", "25", "82", "MLN"] #lista de sedes a consultar, revisar documentación para tener una lista completa
+
+#Datos para notificación de correo electrónico
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"
+receiver_email = "your@gmail.com"
+password = "yourpassword"
